1206cb10fc6e69408bed1183752405d98380efaa
unknown <aztoorabally7447@gmail.com>
2022-06-08 14:00:53 +0200

    svg

---
+++ index.js
@@ -0,0 +1,10 @@
+function add(num1 , num2){
+    // return parseInt(num1) + parseInt(num2);
+    prompt(`enter the first number`);
+    prompt(`enter the first number`);
+    prompt(`enter the first number`);
+    prompt(`enter a second number`);
+    alert(`resultant is ` + ans);
+};
+add();
+document.write(`kettle`);
